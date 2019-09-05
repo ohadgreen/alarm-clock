f159b57322f7b7a0a07dba6dc507f1ec633a7238
Ohad Green <ohad.green@odysii.com>
2019-09-05 11:05:41 +0300

    alarm settings css

--- src/app/setting/AlarmSetting.js
+++ src/app/setting/AlarmSetting.js
@@ -48,28 +48,28 @@ class AlarmSetting extends React.Component {
         <Modal open={this.state.settingModal} size={"tiny"}>
           <Modal.Content>
             <div className="alarm-setting-container">
-            <div className="alarm-setting-header">Set Alarm Time:</div>
-            <div className="set-hours-dd">
-            <Dropdown
-              placeholder={"hours"}
-              onChange={this.props.setHours}
-              options={this.dropDownNumberOptions(23)}
-              scrolling
-            />
-            </div>
-            <div className="set-minutes-dd">
-            <Dropdown
-              placeholder={"minutes"}
-              onChange={this.props.setMinutes}
-              options={this.dropDownNumberOptions(59)}
-              scrolling
-            />
-            </div>
-            <div className="alarm-set-button">
-            <Button size="tiny" color="green" disabled={this.props.setDisabled} onClick={this.setAlarmAndCloseModal}>
-              Set
-            </Button>
-            </div>
+              <div className="alarm-setting-header">Set Alarm Time:</div>
+              <div className="set-hours-dd">
+              <Dropdown
+                placeholder={"hours"}
+                onChange={this.props.setHours}
+                options={this.dropDownNumberOptions(23)}
+                scrolling
+              />
+              </div>
+              <div className="set-minutes-dd">
+              <Dropdown
+                placeholder={"minutes"}
+                onChange={this.props.setMinutes}
+                options={this.dropDownNumberOptions(59)}
+                scrolling
+              />
+              </div>
+              <div className="alarm-set-button">
+              <Button size="tiny" color="green" disabled={this.props.setDisabled} onClick={this.setAlarmAndCloseModal}>
+                Set
+              </Button>
+              </div>
             </div>
           </Modal.Content>
         </Modal>
